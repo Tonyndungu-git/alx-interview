61edb852752de1acb2ff1080d6fb1385dd2e7a22
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-10-14 16:21:12 +0300

    unlock boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+''' can unlock boxes '''
+
+
+def canUnlockAll(boxes):
+    ''' can unlock boxes '''
+    visited = set()
+
+    stack = [0]
+
+    visited.add(0)
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key not in visited and key < len(boxes):
+                visited.add(key)
+                stack.append(key)
+
+    return len(visited) == len(boxes)
